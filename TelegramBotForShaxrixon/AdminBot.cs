f77dd5d06bb1c3614f67db59f33449ab684660a6
iavazov <CSharpLesson>
2020-12-24 00:47:44 +0500

    added

---
+++ TelegramBotForShaxrixon/AdminBot.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelegramBotForShaxrixon
+{
+    public class AdminBot
+    {
+        
+    }
+}
---
+++ TelegramBotForShaxrixon/BotInlinesComponent/Inlines.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TelegramBotForShaxrixon.BotInlinesComponent
+{
+    public  static class Inlines
+    {
+        public async static Task<bool> SendInline(InlineColums column, TelegramBotClient bot) 
+        {
+            try
+            {
+                var inlines = new List<InlineKeyboardButton[]>();
+                foreach(var item in column.CallBacks)
+                    inlines.Add(new[] { InlineKeyboardButton.WithCallbackData($"{ item.CallBackName}", item.CallBackData) });
+
+                var inlineKeyboard = new InlineKeyboardMarkup(inlines);
+                bot.EditMessageTextAsync(column.ChatId, messageId: column.MessageId, column.TextMessage, replyMarkup: inlineKeyboard);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);                
+            }
+            return true;
+        }
+    }
+
+    public class InlineColums
+    { /// <summary>
+      /// 
+      /// </summary>
+        /// <summary>
+        /// 
+        /// </summary>
+        public int ChatId { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int MessageId { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string TextMessage { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<CallBack> CallBacks { get; set; }
+
+       
+    }
+    public class CallBack 
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string CallBackData { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string CallBackName { get; set; }
+    }
+}
